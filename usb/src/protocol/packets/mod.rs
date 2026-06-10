a17dd7866792c69f7e8b8f30f61d2116464d8ecc
Malte Linke <me@parzival.space>
2026-06-10 17:21:33 +0200

    add `HandshakePacket` support to packet parser

--- usb/src/protocol/packets/mod.rs
+++ usb/src/protocol/packets/mod.rs
@@ -1,15 +1,17 @@
 use nom::IResult;
 use crate::protocol::packets::device_status_packet::DeviceStatusPacket;
+use crate::protocol::packets::handshake_packet::HandshakePacket;
 use crate::protocol::packets::property_patch_packet::PropertyPatchPacket;
 use crate::protocol::Parseable;
 
 pub mod device_status_packet;
 pub mod property_patch_packet;
-
+pub mod handshake_packet;
 
 pub(crate) enum Packet {
     DeviceStatus(DeviceStatusPacket),
     PropertyPatch(PropertyPatchPacket),
+    Handshake(HandshakePacket),
     Unknown(Vec<u8>),
 }
 
@@ -19,6 +21,8 @@ pub(crate) fn parse_packet(bytes: &[u8]) -> IResult<&[u8], Packet> {
             .map(|result| (result.0, Packet::DeviceStatus(result.1))),
         _ if PropertyPatchPacket::can_parse(bytes) => PropertyPatchPacket::parse(bytes)
             .map(|result| (result.0, Packet::PropertyPatch(result.1))),
+        _ if HandshakePacket::can_parse(bytes) => HandshakePacket::parse(bytes)
+            .map(|result| (result.0, Packet::Handshake(result.1))),
         _ => Ok((&[], Packet::Unknown(bytes.to_vec()))),
     }
 }
