e2ac1a5574a5609d61937e24c5b0a0708a75d4c1
Malte Linke <me@parzival.space>
2026-08-13 05:44:40 +0200

    Migrate from Slint to Tauri and initialize Vue-based UI framework

--- app/build.rs
+++ app/build.rs
@@ -1,13 +1,3 @@
-use std::collections::HashMap;
-use std::path::PathBuf;
-use slint_build::CompilerConfiguration;
-
 fn main() {
-    let library = HashMap::from([
-        ("lucide".into(), PathBuf::from(lucide_slint::lib()))
-    ]);
-
-    let config = CompilerConfiguration::default()
-        .with_library_paths(library);
-    slint_build::compile_with_config("./ui/app-window.slint", config).unwrap();
+    tauri_build::build();
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,4 @@
-use slint::PlatformError;
-
-slint::include_modules!();
-
-fn main() -> Result<(), PlatformError> {
-    let main_window = MainWindow::new()?;
-
-    main_window.run()
+fn main() -> tauri::Result<()> {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
 }
