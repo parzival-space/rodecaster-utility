c3ca2121596ad25aeb36074c35430bc913c2d86f
Malte Linke <me@parzival.space>
2026-08-13 05:53:27 +0200

    Work around GTK WebKit bug on Linux by disabling DMABUF renderer

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,13 @@
+use std::env;
+
 fn main() -> tauri::Result<()> {
+    unsafe {
+        // under wayland on linux gtkwebkit has a bug that causes the app to crash on startup,
+        // setting the following flag disables the DMABUG renderer to work around this issue.
+        #[cfg(target_os = "linux")]
+        env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
+
     tauri::Builder::default()
         .run(tauri::generate_context!())
 }
