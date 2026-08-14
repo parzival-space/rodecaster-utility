16285ae977ee8ec0a2916ea3ccc2efadcc1dca83
Malte Linke <me@parzival.space>
2026-08-14 23:42:09 +0200

    Integrate custom `TitleBar` component, adjust window configuration, and add support for JSON5 configuration handling

--- app/ui/src/App.vue
+++ app/ui/src/App.vue
@@ -1,6 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TitleBar from "@/components/TitleBar.vue";
+</script>
 
 <template>
+  <TitleBar />
+
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
@@ -8,4 +12,6 @@
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
