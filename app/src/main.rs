315e98f92435fa0ed3a7d48ce74614119ee0da7b
Malte Linke <me@parzival.space>
2026-08-17 13:29:15 +0200

    Migrate UI framework from eframe to Vizia and update dependencies

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,41 +1,63 @@
-use eframe::egui::ViewportBuilder;
-use eframe::{NativeOptions, SurfaceConfig, WgpuConfiguration};
-use eframe::wgpu::PresentMode;
 use log::{LevelFilter, SetLoggerError};
 use simplelog::{Config, TermLogger};
+use vizia::ApplicationError;
+use vizia::context::EventContext;
+use vizia::events::Event;
+use vizia::prelude::{ActionModifiers, Alignment, Application, Color, DataContext, EmitContext, Gradient, HStack, HorizontalPositionKeyword, Label, LayoutModifiers, Length, LineDirection, LinearGradient, Model, Percentage, Pixels, Signal, SignalGet, SignalUpdate, StyleModifiers, VStack, WindowModifiers};
+use vizia::style::VerticalPositionKeyword::Bottom;
+use vizia::vg::shaders::linear_gradient;
+use vizia::views::Button;
 use crate::ui::app::App;
 
 mod ui;
 
 #[derive(Debug)]
-enum RodeCasterUtilityError {
-    EframeError(eframe::Error),
+enum AppError {
+    ApplicationError(ApplicationError),
     SetLoggerError(SetLoggerError)
 }
 
-fn main() -> Result<(), RodeCasterUtilityError> {
+#[derive(Debug, Default)]
+struct AppData {
+    count: Signal<i32>
+}
+
+enum AppEvent {
+    Increment,
+    Decrement
+}
+
+impl Model for AppData {
+    fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
+        event.map(|app_event, _| match app_event {
+            AppEvent::Increment => self.count.update(|count| *count += 1),
+            AppEvent::Decrement => self.count.update(|count| *count -= 1)
+        })
+    }
+}
+
+fn main() -> Result<(), AppError> {
     TermLogger::init(LevelFilter::Debug, Default::default(), Default::default(), Default::default())
-        .map_err(|err| RodeCasterUtilityError::SetLoggerError(err))?;
+        .map_err(|err| AppError::SetLoggerError(err))?;
 
-    let native_options = NativeOptions {
-        viewport: ViewportBuilder::default()
-            .with_decorations(true)
-            .with_min_inner_size([1200.0, 720.0])
-            .with_inner_size([1200.0, 720.0]),
-        wgpu_options: WgpuConfiguration {
-            surface: SurfaceConfig {
-                present_mode: PresentMode::AutoVsync,
-                desired_maximum_frame_latency: None,
-            },
-            ..Default::default()
-        },
+    Application::new(|ctx| {
+        AppData::default().build(ctx);
 
-        ..Default::default()
-    };
-    eframe::run_native(
-        "RODECaster Utility",
-        native_options,
-        Box::new(|_cc| Ok(Box::<App>::default()))
-    )
-        .map_err(|err| RodeCasterUtilityError::EframeError(err))
+        HStack::new(ctx, |ctx| {
+            Button::new(ctx, |ctx| Label::new(ctx, "Increment"))
+                .on_press(|ctx| ctx.emit(AppEvent::Increment));
+
+            Label::new(ctx, ctx.data::<AppData>().count);
+
+            Button::new(ctx, |ctx| Label::new(ctx, "Decrement"))
+                .on_press(|ctx| ctx.emit(AppEvent::Decrement));
+        })
+            .alignment(Alignment::Center)
+            .horizontal_gap(Pixels(50.0));
+
+    })
+        .title("Hello World")
+        .inner_size((400, 100))
+        .run()
+        .map_err(|err| AppError::ApplicationError(err))
 }
